ea07575de140d073d14c943298e7a5c866729f96
Alexander Kuttig <alexander.kuttig@horizon-alpha.com>
2024-11-20 16:23:23 +0100

    changed wording

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hallo!</ThemedText>
+        <ThemedText type="title">Hallo Horizon Alpha!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
